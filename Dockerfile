7ef461cbe8dc1eb2d0c625a79a8a133688369878
Daniel Tedman <dbtedman@gmail.com>
2020-07-12 17:43:10 +1000

    Setup local wp install for setup

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM wordpress
+
+COPY ./bin/docker-setup /usr/local/bin/setup
+COPY ./ /var/www/html/wp-content/plugins/scrutinize-wp/
+
+RUN apt-get update \
+  && apt-get install -y less \
+  && curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+    --output /usr/local/bin/wp-cli \
+  && chmod +x /usr/local/bin/wp-cli
--- main.php
+++ main.php
@@ -1,4 +1,17 @@
 <?php
+declare(strict_types=1);
+
+/**-----------------------------------------------------------------------------
+ *
+ * Author URI:      https://danieltedman.com
+ * Author:          Daniel Tedman
+ * Description:     Scrutinize your WordPress deployment to ensure it remains secure, accessible, and performant.
+ * Plugin Name:     WordPress Actions Audit
+ * Plugin URI:      https://github.com/dbtedman/scrutinize-wp
+ * Text Domain:     scrutinize-wp
+ * Version:         0.1.0
+ *
+ *----------------------------------------------------------------------------*/
 
 if (is_readable(__DIR__ . '/vendor/autoload.php')) {
     require __DIR__ . '/vendor/autoload.php';
